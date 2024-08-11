6a8a333ad78a97d6bc3a59de951380325a05e3ff
Apple Andrei <a.gaitanaru@yahoo.com>
2024-08-11 18:35:55 +0300

    add ci

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+
+describe("Whatever", () => {
+  it("should do something", () => {
+    // Test implementation goes here
+    expect(true).toBe(true);
+  });
+});
